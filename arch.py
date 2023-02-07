f10ec88cb15d2f14a19400a2cd2bdcf2bebbc0e0
eduardzamfir <eduard.zamfir2013@gmail.com>
2023-02-07 10:36:44 +0100

    fix arch definition

--- arch.py
+++ arch.py
@@ -1002,9 +1002,9 @@ def srmodel():
     window_size = 8
     height = (1024 // upscale // window_size + 1) * window_size
     width = (720 // upscale // window_size + 1) * window_size   
-    model = Swin2SR(upscale=2, img_size=(height, width),
-                   window_size=window_size, img_range=1., depths=[6, 6, 6, 6],
-                   embed_dim=60, num_heads=[6, 6, 6, 6], mlp_ratio=2, upsampler='pixelshuffledirect')
+    model = Swin2SR(upscale=upscale, in_chans=3, img_size=64, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='pixelshuffle', resi_connection='1conv')
     return model
 
 
